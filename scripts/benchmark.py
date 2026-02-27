e3f497508273b932d5e524688e033f2738bb3ad7
Fredrik Sundt-Hansen <fredrik.sh.norge@gmail.com>
2026-02-27 11:57:39 +0100

    feat: add remaining datasets from Mags article

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -16,7 +16,16 @@ datasets = {
     "small": [
         ("https://snap.stanford.edu/data/as-caida20071105.txt.gz", "as-caida20071105.txt"),
         ("https://snap.stanford.edu/data/email-Enron.txt.gz", "Email-Enron.txt"),
-        ("https://snap.stanford.edu/data/loc-brightkite_edges.txt.gz", "Brightkite_edges.txt")
+        ("https://snap.stanford.edu/data/loc-brightkite_edges.txt.gz", "Brightkite_edges.txt"),
+        ("https://snap.stanford.edu/data/email-EuAll.txt.gz", "Email-EuAll.txt"),
+        ("https://snap.stanford.edu/data/soc-Slashdot0902.txt.gz", "Slashdot0902.txt"),
+        ("https://snap.stanford.edu/data/bigdata/communities/com-dblp.ungraph.txt.gz", "com-dblp.ungraph.txt")
+    ],
+    "large": [
+        ("https://snap.stanford.edu/data/amazon0601.txt.gz", "amazon0601.txt"),
+        ("https://snap.stanford.edu/data/bigdata/communities/com-youtube.ungraph.txt.gz", "com-youtube.ungraph.txt"),
+        ("https://snap.stanford.edu/data/as-skitter.txt.gz", "as-skitter.txt"),
+        ("https://snap.stanford.edu/data/bigdata/communities/com-lj.ungraph.txt.gz", "com-lj.ungraph.txt")
     ]
 }
 
